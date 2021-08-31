b47a2de5b31a014e7a9b0d8be13ebcd2db742a64
luvletter2333 <luvletter2333@gmail.com>
2021-09-01 01:08:54 +0800

    feat: Remove Credit

--- luci-app-openclash/luasrc/model/cbi/openclash/client.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/client.lua
@@ -124,10 +124,6 @@ o.write = function()
   SYS.call("/etc/init.d/openclash stop >/dev/null 2>&1 &")
 end
 
-d = Map("openclash")
-d.title = translate("Credits")
-d.pageaction = false
-d:section(SimpleSection).template  = "openclash/developer"
 
 
 m:append(Template("openclash/toolbar_show"))
